fa943c3eab60a38b628eeadc74285d13252ac36d
Guwel <guwel@yandex.ru>
2022-12-14 13:32:17 +0500

    add homework

---
+++ Homework/Task 41/Program.cs
@@ -0,0 +1,14 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.Clear();
+Console.WriteLine("Введите числа, по завершении набора чисел, наберите 0: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+while (n!= 0)
+{
+    if (n>0)
+    count++;
+    n = Convert.ToInt32(Console.ReadLine());
+}
+
+Console.WriteLine($"Количество чисел, больше 0: {count}");
